6a5691c6826926f960f64dd9ee38e248ce4a40f0
imber <a1157911285@163.com>
2023-12-21 15:05:25 +0800

    📦 NEW: editor template

---
+++ editor-demos/tiptap-templates-main/templates/next-block-editor-app/src/app/[room]/page.tsx
@@ -0,0 +1,112 @@
+'use client'
+
+import { TiptapCollabProvider } from '@hocuspocus/provider'
+import 'iframe-resizer/js/iframeResizer.contentWindow'
+import { useSearchParams } from 'next/navigation'
+import { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react'
+import * as Y from 'yjs'
+
+import { BlockEditor } from '@/components/BlockEditor'
+import { createPortal } from 'react-dom'
+import { Surface } from '@/components/ui/Surface'
+import { Toolbar } from '@/components/ui/Toolbar'
+import { Icon } from '@/components/ui/Icon'
+
+export interface AiState {
+  isAiLoading: boolean
+  aiError?: string | null
+}
+
+const useDarkmode = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    const handleChange = () => setIsDarkMode(mediaQuery.matches)
+    mediaQuery.addEventListener('change', handleChange)
+    return () => mediaQuery.removeEventListener('change', handleChange)
+  }, [])
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDarkMode)
+  }, [isDarkMode])
+
+  const toggleDarkMode = useCallback(() => setIsDarkMode(isDark => !isDark), [])
+  const lightMode = useCallback(() => setIsDarkMode(false), [])
+  const darkMode = useCallback(() => setIsDarkMode(true), [])
+
+  return {
+    isDarkMode,
+    toggleDarkMode,
+    lightMode,
+    darkMode,
+  }
+}
+
+export default function Document({ params }: { params: { room: string } }) {
+  const { isDarkMode, darkMode, lightMode } = useDarkmode()
+  const [provider, setProvider] = useState<TiptapCollabProvider | null>(null)
+  const [token, setToken] = useState<string | null>(null)
+  const searchParams = useSearchParams()
+
+  const hasCollab = parseInt(searchParams.get('noCollab') as string) !== 1
+
+  const { room } = params
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await (
+        await fetch('/api/collab-auth', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      ).json()
+
+      const { token } = data
+
+      // set state when the data received
+      setToken(token)
+    }
+
+    dataFetch()
+  }, [])
+
+  const ydoc = useMemo(() => new Y.Doc(), [])
+
+  useLayoutEffect(() => {
+    if (hasCollab && token) {
+      setProvider(
+        new TiptapCollabProvider({
+          name: `${process.env.NEXT_PUBLIC_CLOUD_PREFIX}${room}`,
+          appId: process.env.NEXT_PUBLIC_TIPTAP_COLLAB_APP_ID ?? '',
+          token,
+          document: ydoc,
+        }),
+      )
+    }
+  }, [setProvider, token, ydoc, room, hasCollab])
+
+  if (hasCollab && (!token || !provider)) return
+
+  const DarkModeSwitcher = createPortal(
+    <Surface className="flex items-center gap-1 fixed bottom-6 right-6 z-[99999] p-1">
+      <Toolbar.Button onClick={lightMode} active={!isDarkMode}>
+        <Icon name="Sun" />
+      </Toolbar.Button>
+      <Toolbar.Button onClick={darkMode} active={isDarkMode}>
+        <Icon name="Moon" />
+      </Toolbar.Button>
+    </Surface>,
+    document.body,
+  )
+
+  return (
+    <>
+      {DarkModeSwitcher}
+      <BlockEditor hasCollab={hasCollab} ydoc={ydoc} provider={provider} />
+    </>
+  )
+}
