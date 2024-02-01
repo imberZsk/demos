b1362867e587a3570e0eaec8f8c50b2a38ce8171
imber <a1157911285@163.com>
2024-02-01 10:40:58 +0800

    feat: webpack

---
+++ webpack-learn/babel.config.js
@@ -0,0 +1,11 @@
+// babel.config.js
+
+module.exports = {
+  presets: [
+    // 配置规则
+    '@babel/preset-env',
+    '@vue/babel-preset-jsx'
+  ],
+  // 配置插件
+  plugins: ['@babel/plugin-transform-runtime']
+}
