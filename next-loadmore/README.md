9c86a55d8cb4c2b26622cce25d3cc42c2ccbc3ef
imber <a1157911285@163.com>
2024-04-01 09:28:08 +0800

    feat: loadmore

--- next-loadmore/README.md
+++ next-loadmore/README.md
@@ -2,8 +2,12 @@
 
 - 下拉加载，不能直接把数据放到 `useState` 里，放到 `useEffect` 里，也可以实现根据 `id` 渲染
 
-- SSG 多语言
+- SSG 多语言，可能会和 CSR 起冲突
 
-- toast
+- toast 组件
 
-- 气泡
+- 气泡组件
+
+- shadcn
+
+- storybook
--- next-loadmore/src/app/[lng]/test/button.tsx
+++ next-loadmore/src/app/[lng]/test/button.tsx
@@ -1,17 +1,20 @@
 'use client'
-
+import { toast } from 'sonner'
 import { useState } from 'react'
 
 const Button = () => {
   const [state, setState] = useState(1)
   return (
-    <div
-      onClick={() => {
-        setState(state + 1)
-      }}
-    >
-      点击{state}
-    </div>
+    <>
+      <div
+        onClick={() => {
+          // setState(state + 1)
+          toast('Event has been created')
+        }}
+      >
+        点击{state}
+      </div>
+    </>
   )
 }
 
