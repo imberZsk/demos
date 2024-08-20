5814a21720eb2a28093b87a26953ab0d143d7bc7
imber <a1157911285@163.com>
2024-08-20 09:45:09 +0800

    delete wechat

--- wechat/miniprogram/assets/libs/utils/download.ts
+++
@@ -1,9 +0,0 @@
-export function downLoadFileApi(url: string): Promise<{ tempFilePath: string }> {
-    return new Promise((resolve, reject) => {
-        wx.downloadFile({
-            url,
-            success: resolve,
-            fail: reject
-        });
-    })
-}
